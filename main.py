2016ad0f396c44fa58dcb1e080e2a8ae73890208
cliffclimber-721 <cliffclimber@g.skku.edu>
2022-12-30 14:44:47 +0900

    📝 Login functions 📝

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
 
 st.set_page_config(
     page_title="#my_storage",
@@ -9,3 +11,18 @@ st.markdown("""
 ### #my_storage 에서 간단한 앨범을 만들어볼 수 있어요❗️
 **👈 메뉴도 함께 참고해주세요❗️**
 """)
+
+hashpwd = stauth.Hasher(['0721', '0123']).generate
+
+with open("/Users/skywalker721/Desktop/github_file/mystorage/config.yaml") as file:
+    config = yaml.load(file, Loader=yaml.Loader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
--- pages/upload.py
+++ pages/upload.py
@@ -1,11 +1,21 @@
 import streamlit as st
 import datetime
 
-st.set_page_config(page_title="my_upload", page_icon="📷")
-st.title("Upload your memories 😆")
-uploadPhoto = st.file_uploader("UPLOAD", accept_multiple_files=True, type="png")
+st.title("#my_storage")
+photoBtn = st.button("Press to upload❗️")
+uploadPhoto = st.file_uploader("Press to upload your memories 😆", accept_multiple_files=True, type="png")
+defaultDate = datetime.date(2015, 1, 1)
 
-if uploadPhoto:
-    dateBtn = st.button("Your date?")
-    if dateBtn:
-        uploadDate = st.date_input("Date input", datetime.date(2022, 12, 1))
+if photoBtn:
+    uploadPhoto
+    if uploadPhoto:
+        with st.form("my_form"):
+            st.write("Please select your date")
+            uploadDate = st.date_input("Date input", value=defaultDate)
+            submitted = st.form_submit_button("Submit")
+
+            if submitted:
+                if uploadDate == defaultDate:
+                    st.write("Date not changed, Please select your date.")
+                else:
+                    st.write("Date changed. Write your code below what you want.")
